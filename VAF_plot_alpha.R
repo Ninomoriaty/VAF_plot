34e5af48a1950f4bdba62820d40a9a39d9577a1c
Wang Chengwei <504430784@qq.com>
2019-03-31 03:48:58 +0800

    vline ofa+normal

--- VAF_plot_alpha.R
+++ VAF_plot_alpha.R
@@ -3,42 +3,71 @@ library(ggplot2)
 library(maftools)
 library(ggridges)
 library(ggsci)
+library(dplyr)
 
 ############ Using maftools ###############
 # Usage: VAF_plot("maf_file_dir", "Sample_Option")
 
 # VAF drawer
-VAF_OFA <- function(cluster_all, theme_option){
+VAF_OFA <- function(cluster_all, theme_option, tsb_ls, sample_option){
   VAF_ofa_cha = paste("ggplot(cluster_all, aes(x=VAF, y=Tumor_Sample_Barcode)) +
     theme_bw()+theme(panel.grid=element_blank(),panel.border=element_blank(), axis.line=element_line(size=0.25)) + 
     geom_point(aes(x=VAF, y=Tumor_Sample_Barcode, color = cluster), alpha = 0.5) +
-    geom_density_ridges(color = \"cadetblue3\", fill = \"whitesmoke\", calc_ecdf = TRUE, alpha = 0.5) + ", 
+    geom_density_ridges(color = \"cadetblue3\", fill = \"whitesmoke\", calc_ecdf = TRUE, alpha = 0.5) + ",
+    VAF_vline_ofa(cluster_all, tsb_ls, sample_option), 
     "scale_color_", theme_option, "() + scale_fill_", theme_option, "()", sep="")
   eval(parse(text = VAF_ofa_cha))
 }
 
-VAF_draw <- function(cluster_mt, theme_option){
+VAF_draw <- function(cluster_mt, theme_option, sample_option){
   picv <- ggplot(cluster_mt, aes(x = VAF)) + geom_line(size = 1, colour = "cadetblue3", stat = "density")
   VAF_draw_cha = paste("ggplot(cluster_mt, aes(x = VAF)) + 
     theme_bw()+theme(panel.grid=element_blank(),panel.border=element_blank(), axis.line=element_line(size=0.25)) + 
     geom_line(size = 1, colour = \"cadetblue3\", stat = \"density\") + geom_rug(aes(y = 0, colour = cluster), sides = \"b\") + ", 
-    VAF_vline(cluster_mt, picv),
+    VAF_vline(cluster_mt, picv, tsb_ls, sample_option),
     "scale_color_", theme_option, "() + scale_fill_", theme_option, "()", sep="")
   eval(parse(text = VAF_draw_cha))
 }
 
 # VAF draw vlines
-VAF_vline <- function(cluster_mt, pic){
+VAF_vline <- function(cluster_mt, pic, tsb_ls, sample_option, tsb){
   VAF_vline_cha <- ""
   cluster_ls <- unique(cluster_mt$cluster)
   density_info <- data.frame(layer_data(pic))
+  ymin <- min(density_info$y)
+  gr <- ggplot(cluster_all, aes(x = VAF, y = Tumor_Sample_Barcode)) + geom_density_ridges(rel_min_height = 0.01) 
+  ingredients <- ggplot_build(gr) %>% purrr::pluck("data", 1)
+  density_lines <- ingredients %>% group_by(group) %>% filter(density == max(density)) %>% ungroup()
   for (cluster_name in cluster_ls){
     x_end <- max(cluster_mt[which(cluster_mt$cluster == cluster_name)]$VAF)
     x_end_alter <- density_info$x[which.min(abs(outer(density_info$x,x_end,FUN="-")))]
     y_end <- density_info$y[which(density_info$x == x_end_alter)]
-    VAF_vline_cha <- paste(VAF_vline_cha, "geom_segment(aes(x = ", x_end_alter,", xend = ", x_end_alter, ", y = 0, yend = ", y_end,"), size = 0.3, colour=\"cadetblue3\", linetype=\"dashed\") + ",sep="")
+    if (sample_option == "OFA"){
+      VAF_vline_cha <- paste(VAF_vline_cha, "geom_segment(data = cluster_mt_", which(tsb_ls == tsb)," ,aes(x = ", x_end_alter,", xend = ", x_end_alter, ", y = ", which(tsb_ls == tsb),", yend = ", ymin+density*scale*iscale,"), size = 0.3, colour=\"cadetblue3\", linetype=\"dashed\") + ",sep="")
+    }else{
+      VAF_vline_cha <- paste(VAF_vline_cha, "geom_segment(aes(x = ", x_end_alter,", xend = ", x_end_alter, ", y = 0, yend = ", y_end,"), size = 0.3, colour=\"cadetblue3\", linetype=\"dashed\") + ",sep="")
+    }
   }
   VAF_vline_cha
+}
+
+
+# VAF draw vlines for ofa
+VAF_vline_ofa <- function(cluster_all, tsb_ls, sample_option){
+  VAF_vline_ofa <- ""
+  for (tsb in tsb_ls$samples){
+    VAF_vline_cha <- ""
+    x_end_ls <- data.frame()
+    y_end_ls <- data.frame()
+    cluster_mt <- cluster_all[which(cluster_all$Tumor_Sample_Barcode == tsb)]
+    colnames(cluster_mt)[6] = "VAF"
+    cluster_ls <- unique(cluster_mt$cluster)
+    picv <- ggplot(cluster_mt, aes(x = VAF)) + geom_line(size = 1, colour = "cadetblue3", stat = "density")
+    density_info <- data.frame(layer_data(picv))
+    VAF_vline_cha <- VAF_vline(cluster_mt, picv, tsb_ls, sample_option, tsb)
+    VAF_vline_ofa <- paste(VAF_vline_ofa, VAF_vline_cha)
+  }
+  VAF_vline_ofa
 }
 
 # maftools Clusters(could not be used because of the specific position of the point)
@@ -72,19 +101,23 @@ VAF_plot <-function(maf_file, sample_option = "OFA", theme_option = "aaas", file
     for (counter_mt in 1:length(tsb_ls[,1])){
       for (sample_name_mt in tsb_ls){
         sample_mt <- vaf_input_mt[which(vaf_input_mt$Samples %in% as.character(sample_name_mt)[counter_mt]),]
-        cluster_mt = inferHeterogeneity(maf = laml, tsb = as.character(sample_mt[1,3]), vafCol = 'VAF', useSyn = TRUE)$"clusterData"
-        cluster_all <- rbind(cluster_all, cluster_mt)
+        cluster_mt_cha <- paste("cluster_mt_", counter_mt,"= inferHeterogeneity(maf = laml, tsb = as.character(sample_mt[1,3]), vafCol = \'VAF\', useSyn = TRUE)$\"clusterData\"", sep ="")
+        eval(parse(text = cluster_mt_cha))
+        cluster_mt_cha <- paste("colnames(cluster_mt_", counter_mt, ")[6] = \"VAF\"",sep ="")
+        eval(parse(text = cluster_mt_cha))
+        cluster_mt_cha <- paste("cluster_all <- rbind(cluster_all, cluster_mt_", counter_mt, ")",sep ="")
+        eval(parse(text = cluster_mt_cha))
         }
     }
     colnames(cluster_all)[6] = "VAF"
-    pic <- VAF_OFA(cluster_all, theme_option)
+    pic <- VAF_OFA(cluster_all, theme_option, tsb_ls, sample_option)
     ggsave(pic, filename =  paste(patientID, "_VAF_Cluster",".", file_format, sep=""), width = 12, height = 9)
   } else {
   # specific sample
     sample_mt <- vaf_input_mt[which(vaf_input_mt$Samples %in% sample_option),]
     cluster_mt = inferHeterogeneity(maf = laml, tsb = as.character(sample_mt[1,3]), vafCol = 'VAF', useSyn = TRUE)$"clusterData"
     colnames(cluster_mt)[6] = "VAF"
-    pic <- VAF_draw(cluster_mt, theme_option)
+    pic <- VAF_draw(cluster_mt, theme_option, sample_option)
     ggsave(pic, filename =  paste(sample_option,"_VAF_Cluster",".", file_format,sep=""), width = 12, height = 9)
   }
 }
